6f03ecb241bdd3135bdad258dd47b2031f0592f3
rkarina <rahmadhysk@gmail.com>
2019-08-10 15:41:44 +1000

    created app with create-react-app and added files for homepage

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuItem = () => (
+    <div className="menu-item">
+        <div className="content">
+            <div className="title">Hats</div>
+            <div className="subtitle">SHOP NOW</div>
+        </div>
+    </div>
+)
+
+export default MenuItem; 
