5881ddd9e93dc0de772a590189b0f5c119315fd1
rkarina <rahmadhysk@gmail.com>
2019-12-02 23:32:27 +1100

    Added collection selector.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -5,6 +5,7 @@ import CollectonItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => {
+    console.log("Title: ", title);
     return (
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
